042833cb84deb262a5a633cfea8fc7ea99fe3bbe
Timm Kent <marcfelden@me.com>
2023-12-04 06:39:57 +0100

    first commit

--- lib/src/tkuuid.dart
+++ lib/src/tkuuid.dart
@@ -9,6 +9,8 @@ class TKUUID {
   }
   SharedPreferences? shared;
   String? _tkuuid;
+  bool? _isrestart;
+
   static initialize() async {
     final shared = await SharedPreferences.getInstance();
     final savedTKUUID = shared.getString('tkUUID');
@@ -16,13 +18,20 @@ class TKUUID {
       final tkUUID = const Uuid().v1();
       shared.setString('tkUUID', tkUUID);
       _singleton._tkuuid = tkUUID;
+      _singleton._isrestart = true;
     } else {
       _singleton._tkuuid = savedTKUUID;
+      _singleton._isrestart = false;
     }
   }
 
   static String get tkuuid {
     assert(_singleton._tkuuid != null, "TKUUID not initialized.");
     return _singleton._tkuuid!;
+  }
+
+  static bool get isRestart {
+    assert(_singleton._isrestart != null, "TKUUID not initialized.");
+    return _singleton._isrestart!;
   }
 }
